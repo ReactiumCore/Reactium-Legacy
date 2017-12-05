e514492e0b5c0a1834b27f013ab069c1fff2d26a
John Dillick <jdillick@resourceammirati.com>
2017-12-05 12:23:28 -0500

    Add comments for a example component route.

--- src/app/components/Test/route.js
+++ src/app/components/Test/route.js
@@ -2,9 +2,15 @@ import Test from './index';
 import { actions } from 'appdir/app';
 
 export default {
-    key: 'Test',
+    // Route pattern
     path: '/',
+
+    // Route should be exact
     exact: true,
+
+    // the component to load for this route
     component: Test,
+
+    // load callback should return thunk that uses route params.
     load: params => actions.Test.mount(params),
 };
