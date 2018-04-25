682c5e3961cd76488cc3a77aae25d7a5eb012c10
John Dillick <jdillick@resourceammirati.com>
2018-04-25 17:39:36 -0400

    Updated route observer to handle URL search string.

--- src/app/components/Router/actions.js
+++ src/app/components/Router/actions.js
@@ -1,21 +1,23 @@
 import { actionTypes } from 'appdir/app';
+import queryString from 'query-string';
 
 export default {
     updateRoute: (location, route = {}, params) => (dispatch, getState) => {
         const { Router } = getState();
 
-        if ( Router.pathname !== location.pathname ) {
+        if ( typeof window !== 'undefined' && Router.pathname !== location.pathname ) {
             window.scrollTo(0,0);
         }
 
         dispatch({
             type: actionTypes.UPDATE_ROUTE,
             location,
+            params,
         });
 
         // load route data
         if ( 'load' in route ) {
-            dispatch(route.load(params));
+            dispatch(route.load(params, queryString.parse(location.search)));
         }
     },
 };
